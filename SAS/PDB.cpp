145b623f93055deecfa9a9e651cdfdf1e4ca4e49
Ryan Aikman <aikman.ryan@gmail.com>
2016-04-13 19:23:47 -0600

    Gain sucks

---
+++ SAS/PDB.cpp
@@ -0,0 +1,9 @@
+//
+//  PDB.cpp
+//  SAS
+//
+//  Created by Ryan Aikman on 4/13/16.
+//  Copyright © 2016 Gain Hagenau. All rights reserved.
+//
+
+#include "PDB.hpp"
---
+++ SAS/PDB.hpp
@@ -0,0 +1,14 @@
+//
+//  PDB.hpp
+//  SAS
+//
+//  Created by Ryan Aikman on 4/13/16.
+//  Copyright © 2016 Gain Hagenau. All rights reserved.
+//
+
+#ifndef PDB_hpp
+#define PDB_hpp
+
+#include <stdio.h>
+
+#endif /* PDB_hpp */
