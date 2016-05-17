003247769f9c26340e0ee82774cf18263dfc0a94
Ryan Aikman <aikman.ryan@gmail.com>
2016-05-17 00:45:33 -0600

    GridMap

--- SAS/LMDifferential.cpp
+++ SAS/LMDifferential.cpp
@@ -26,16 +26,18 @@ int LMDifferential::GetHeuristic(MapState state){
 //populate the pivot array based on the pivot passed in
 void LMDifferential::BuildPivot(vector<int> &pivotArray, MapState p){
     //set to size of index
+    pivotArray.clear();
     pivotArray.resize(map().getSize());
     
     AStar<MapState, MapAction, GridMaps, GridMaps> AStar;
     //for every valid index run A* and find distance from that state to pivot state
-    for (int i = 0; i < pivotArray.size(); ++i) {
+    for (int i = 0; i < pivotArray.size(); i++) {
         MapState m = map().getMapState(i);
         if (map().isValid(m.x, m.y)) {
             GridMaps grid(map().getMapState(i));
-            AStar.GetPath(grid, p, m, grid);
-            
+            if (AStar.GetPath(grid, p, m, grid)) {
+                pivotArray.push_back(AStar.getCostOfPreviousSolution());
+            }
         }
     }
 }
